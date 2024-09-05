577ea8275149f1143cbaec5b94c905e6bb5a4a34
matyasosvath <matyasosv@gmail.com>
2024-09-05 16:28:43 +0200

    wip

--- puli/models/puli3_gpt_neox.py
+++ puli/models/puli3_gpt_neox.py
@@ -1,3 +1,5 @@
+from typing import Optional, Tuple
+
 from dataclasses import dataclass
 
 import torch
@@ -7,6 +9,7 @@ from torch import Tensor
 
 @dataclass
 class ModelArgs:
+    batch_size: int = 32
     context_length: int = 2048 # block size, seq length
     vocab_size: int = 50048
     dropout: float = 0.0
@@ -60,37 +63,52 @@ class MLP(nn.Module):
         return x
 
 
-class MultiHeadedAttention(nn.Module):
-    def __init__(
-        self,
-        d_in: int,
-        d_out: int,
-        n_heads: int,
-        dropout: float = 0.0,
-        qkv_bias: bool = True,
-    ) -> None:
-
+class KVCache(nn.Module):
+    def __init__(self, batch_size: int, max_seq_length: int, n_heads: int, head_dim: int):
         super().__init__()
 
-        assert d_out % n_heads == 0, "d_model is indivisible by n_heads"
+        cache_shape = (batch_size, max_seq_length, n_heads, head_dim)
+        self.register_buffer('k_cache', torch.zeros(cache_shape))
+        self.register_buffer('v_cache', torch.zeros(cache_shape))
 
-        self.n_heads = n_heads
-        self.head_dim = d_out // n_heads
-        self.d_out = d_out
+    def update(self, input_pos: Tensor, k_val: Tensor, v_val: Tensor) -> Tuple[Tensor, Tensor]:
 
-        self.query_key_value = nn.Linear(d_in, 3 * d_out, bias=qkv_bias)
-        self.dense = nn.Linear(d_out, d_out)
+        # input_pos: [S], k_val: [B, S, H, D]
+        assert input_pos.shape[0] == k_val.shape[1]
 
-        self.attention_dropout = nn.Dropout(dropout)
+        k_out = self.k_cache
+        v_out = self.v_cache
+        k_out[:, input_pos, :, :] = k_val
+        v_out[:, input_pos, :, :] = v_val
 
-    def forward(self, x: torch.Tensor, freqs_cis: torch.Tensor):
+        return k_out, v_out
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, args: ModelArgs) -> None:
+        super().__init__()
+
+        assert args.d_model % args.n_heads == 0, "d_model is indivisible by n_heads"
+
+        self.n_heads = args.n_heads
+        self.head_dim = args.d_model // args.n_heads
+        self.d_model = args.d_model
+
+        self.query_key_value = nn.Linear(args.d_model, 3 * args.d_model, bias=args.qkv_bias)
+        self.dense = nn.Linear(args.d_model, args.d_model)
+
+        self.attention_dropout = nn.Dropout(args.dropout)
+
+        self.kv_cache = KVCache(1, args.context_length, args.n_heads, self.head_dim)
+
+    def forward(self, x: Tensor, freqs_cis: Tensor, input_pos: Tensor):
 
         batch_size, seq_length, d_model = x.shape
 
         # (b, seq_length, d_model) --> (b, seq_length, 3 * d_model)
         qkv = self.query_key_value(x)
 
-        q, k, v = torch.split(qkv, self.d_out, dim=-1)
+        q, k, v = torch.split(qkv, self.d_model, dim=-1)
 
         q = q.view(batch_size, seq_length, self.n_heads, self.head_dim)
         k = k.view(batch_size, seq_length, self.n_heads, self.head_dim)
@@ -99,15 +117,23 @@ class MultiHeadedAttention(nn.Module):
         q = apply_rotary_emb(q, freqs_cis)
         k = apply_rotary_emb(k, freqs_cis)
 
+        # q, k, v = map(lambda x: x.transpose(1, 2), (q, k, v))
+        k, v = self.kv_cache.update(input_pos, k,v)
+
         context_vec = nn.functional.scaled_dot_product_attention(
             q, k, v, attn_mask=None, dropout_p=0.0, is_causal=True
         )
 
-        # combine heads, where self.d_out = self.n_heads * self.head_dim
+        print(f"q: {q.shape}")
+        print(f"k: {k.shape}")
+        print(f"v: {v.shape}")
+        print(f"context_vec.shape: {context_vec.shape}")
+
+        # combine heads, where self.d_model = self.n_heads * self.head_dim
         context_vec = (
             context_vec.transpose(1, 2)
             .contiguous()
-            .view(batch_size, seq_length, self.d_out)
+            .view(batch_size, seq_length, self.d_model)
         )
 
         context_vec = self.dense(context_vec)
@@ -148,18 +174,12 @@ class Block(nn.Module):
         self.post_attention_dropout = nn.Dropout(args.dropout, inplace=False)
         self.post_mlp_dropout = nn.Dropout(args.dropout, inplace=False)
 
-        self.attention = MultiHeadedAttention(
-            d_in=args.d_model,
-            d_out=args.d_model,
-            n_heads=args.n_heads,
-            dropout=args.dropout,
-            qkv_bias=args.qkv_bias
-            )
+        self.attention = MultiHeadedAttention(args=args)
 
         self.mlp = MLP(args.d_model)
 
-    def forward(self, x: torch.Tensor, freqs_cis: torch.Tensor) -> torch.Tensor:
-        x = x + self.post_attention_dropout(self.attention(self.input_layernorm(x), freqs_cis))
+    def forward(self, x: Tensor, freqs_cis: Tensor, input_pos: Tensor) -> Tensor:
+        x = x + self.post_attention_dropout(self.attention(self.input_layernorm(x), freqs_cis, input_pos))
         x = x + self.post_mlp_dropout(self.mlp(self.post_attention_layernorm(x)))
         return x
 
@@ -183,11 +203,11 @@ class Puli3GptNeox(nn.Module):
             self.args.rope_base
         )
 
-    def forward(self, idx: torch.Tensor):
+    def forward(self, idx: Tensor, input_pos: Optional[Tensor] = None):
         x = self.embed_in(idx)
         x = self.embed_dropout(x)
         for layer in self.layers:
-            x = layer(x, self.freqs_cis)
+            x = layer(x, self.freqs_cis, input_pos)
         x = self.final_layer_norm(x)
         logits = self.embed_out(x)
         return logits
