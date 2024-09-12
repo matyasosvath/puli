6d4748ef90d2f82534d0b73507fb573c0d48e01e
matyasosvath <matyasosv@gmail.com>
2024-09-12 11:21:02 +0200

    add utils

--- example/example_text_gen.py
+++ example/example_text_gen.py
@@ -7,13 +7,18 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 puli = load_model("puli2-gpt", device)
 
+prompt = "Elmesélek egy történetet a nyelvtechnológiáról."
+
 print(
     puli.text_completion(
-        "Elmesélek egy történetet a nyelvtechnológiáról.",
-        max_new_tokens=10,
+        prompt,
+        max_new_tokens=100,
         batch_size=1,
         strategy="top_k_sampling",
         temperature=1.3,
         top_k=3,
     )
 )
+
+inputs = puli.tokenizer.encode(prompt).to(device)
+print(f"token/sec: {puli.calculate_token_per_second(inputs, 100)}")
